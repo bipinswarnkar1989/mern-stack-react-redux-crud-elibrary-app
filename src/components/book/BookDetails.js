c38e56bb7df482e0f1a71f0210245cee32601601
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2017-08-17 18:07:22 +0530

    Added pdf view in book details

--- src/components/book/BookDetails.js
+++ src/components/book/BookDetails.js
@@ -1,6 +1,7 @@
 // ./src/components/book/BookDetails.js
 import React from 'react';
-
+import ReactPDF from 'react-pdf';
+import './BookPage.css'
 // const BookDetails = ({book, addToCart}) => {
 //   return (
 //     <div className="media">
@@ -29,19 +30,29 @@ class BookDetails extends React.Component {
   // }
 
 
+
+
 render(){
   const { isFetching, favourites, newFavourite, error } = this.props.favouritesData;
   const  b  = this.props.book;
   return (
-    <div className="media">
-        <div className="media-left">
-          <a href="/#">
-            <img className="media-object" src="http://placehold.it/200x280" alt="Placehold" />
+    <div className="bookDetail">
+        <div className="col-md-6">
+          <a target="__blank" href={`//localhost:8080/${b.fileName}`}>
+            <div className="small_pdf_image">
+              <ReactPDF
+                      file={`http://localhost:8080/${b.fileName}`}
+                      pageIndex={0}
+                      loading="Please wait!" scale={2.0} width={300}
+                      onDocumentLoad={this.onDocumentLoad}
+                      onPageLoad={this.onPageLoad}
+                  />
+            </div>
           </a>
-        </div>{JSON.stringify()}
-        <div className="media-body">
-          <h4 className="media-heading">{b.title}</h4>
-          <ul>
+        </div>
+        <div className="col-md-6">
+          <h4 className="">{b.title}</h4>
+          <ul className="list-group">
             <li><stron>Author: </stron> {b.author}</li>
             <li><stron>Price: </stron> ${b.price}</li>
             <li><stron>Year: </stron> {b.year} </li>
